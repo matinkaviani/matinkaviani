b2389507f33b2d9abf2888ac350f9ee0afa551bf
Matin Kaviani <59339049+whereismatin@users.noreply.github.com>
2023-04-22 16:15:14 +0330

    add footer

---
+++ config/confg.ts
@@ -0,0 +1,12 @@
+const socialLinks = {
+    linkedIn: "https://www.linkedin.com/in/matin-kaviani-3a7755202/",
+    github: "https://github.com/whereismatin",
+    telegram: "https://t.me/matinkaviani",
+    email: "mhp5454@gmail.com"
+}
+
+const config = {
+    socialLinks
+}
+
+export default config;
--- modules/Layout/Footer.tsx
+++ modules/Layout/Footer.tsx
@@ -1,7 +1,22 @@
+import styles from '@/styles/Layout.module.css'
+
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div id={styles.footer}>
+            <div>
+                <div className={styles.logo}>
+                    <svg width="38" height="37" viewBox="0 0 38 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18.1387 26.8521L10.169 2.68258L2.19923 26.8521L18.1387 26.8521Z" stroke="#2C64EF" />
+                        <path d="M10.4746 9.45942L18.4443 33.6289L26.414 9.45942L10.4746 9.45942Z" stroke="#2C64EF" />
+                        <path d="M35.1133 26.8521L27.1436 2.68258L19.1738 26.8521L35.1133 26.8521Z" stroke="#2C64EF" />
+                    </svg>
+                    Matt
+                </div>
+                <div>
+                    FrontEnd Developer
+                </div>
+            </div>
+            <span>are</span>
         </div>
     )
 }
