0b8f25534f20ea40b3799233feba89d02ba08906
Matin Kaviani <59339049+whereismatin@users.noreply.github.com>
2023-04-23 20:57:13 +0330

    complete footer

--- config/confg.ts
+++ config/confg.ts
@@ -2,7 +2,9 @@ const socialLinks = {
     linkedIn: "https://www.linkedin.com/in/matin-kaviani-3a7755202/",
     github: "https://github.com/whereismatin",
     telegram: "https://t.me/matinkaviani",
-    email: "mhp5454@gmail.com"
+    email: "mhp5454@gmail.com",
+    instagram: "https://www.instagram.com/matinkaviiani/",
+    discord: "https://discordapp.com/users/505795959104733199"
 }
 
 const config = {
