91defec0bc27bd011d2b727106a3dadd07cd99cb
Matin Kaviani <59339049+matinkaviani@users.noreply.github.com>
2023-07-10 21:34:47 +0330

    add counter to progress skill bar

--- modules/Skills/ProgressBar/index.tsx
+++ modules/Skills/ProgressBar/index.tsx
@@ -8,19 +8,36 @@ interface ProgressBarProps {
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ skill, progress }) => {
     const progressRef = useRef<HTMLDivElement>(null);
-
+    const [count, setCount] = useState(0);
     useEffect(() => {
         const progressElement = progressRef.current;
         if (progressElement) {
             progressElement.style.width = `${progress}%`;
         }
     }, [progress]);
-
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((prevCount) => {
+                if (prevCount === progress) {
+                    clearInterval(interval); // Stop the interval when the limit is reached
+                    return prevCount;
+                } else {
+                    return prevCount + 1;
+                }
+            });
+        }, 100);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
     return (
         <div className={styles.container}>
             <span className={styles.skillName}>{skill}:</span>
             <div className={styles.progressBar}>
-                <div ref={progressRef} className={styles.progressIndicator}></div>
+                <div ref={progressRef} className={`${styles.progressIndicator} ${styles.progressAnimation}`}></div>
+            </div>
+            <div className="counter" style={{ width: `${(count / progress) * 100}%` }}>
+                {count}
             </div>
         </div>
     );
